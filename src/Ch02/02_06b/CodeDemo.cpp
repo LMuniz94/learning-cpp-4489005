b3aa4758db415972f73f9f7a2946fa88fc057524
LMuniz94 <l.e.muniz94@gmail.com>
2025-06-17 20:22:45 +0000

    preprocessor directives

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -2,10 +2,20 @@
 // Exercise 02_06
 // Preprocessor directives, by Eduardo Corpeño 
 
-#include <iostream>
+#include <iostream> // '<>' means it will look for that file
+#include <string> 
+#include <cstdint> // using C header files 
+
+#define CAPACITY 5000 //macros, here 'CAPACITY' will be replaced by 5000
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "[About to perform addition]" 
+#endif
+    large += small; // shorthand for "large = large + small"
+
     std::cout << std::endl << std::endl;
     return (0);
 }
